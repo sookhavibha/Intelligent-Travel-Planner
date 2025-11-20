75eec3bd512e4490104cfea369b0be17f8742d1d
Vibha Sookha <sookhavibha@gmail.com>
2025-11-20 18:07:02 +0400

    splash screen added

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,37 @@
-import { Text, View } from "react-native";
+import { Image, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
-export default function Index() {
- return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+export default function WelcomeScreen() {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     router.replace("/(auth)/sign-up");
+  //   }, 2500);
+
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+  return (
+    <SafeAreaView className="flex-1 bg-white justify-center items-center px-100">
+      {/* Logo */}
+      <Image
+  source={require("../assets/Altavia logo.png")}
+  className="mb-6"
+  style={{ width: 224, height: 224 }} // twice 112
+  resizeMode="contain"
+/>
+
+      {/* App Name */}
+      <Text className="text-3xl font-bold text-neutral-900 mb-1">
+        Altavia
       </Text>
-    </View>
+
+      {/* Slogan */}
+      <Text className="text-base text-gray-600">
+        Your AI-powered travel planning Assistant"
+      </Text>
+    </SafeAreaView>
   );
 }
